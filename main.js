2691434c8c20644fe6971455b60a54d51aabf385
Capybara <KapibarovKonstantin@gmail.com>
2026-02-28 15:33:21 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
-function main() {
-    fetch('https://raw.githubusercontent.com/Agrizok22507/BylbaConsole/main/gui.js').then(r=>r.text()).then(eval)
-    fetch('https://raw.githubusercontent.com/Agrizok22507/BylbaConsole/main/execute.js').then(r=>r.text()).then(eval)
-    fetch('https://raw.githubusercontent.com/Agrizok22507/BylbaConsole/main/comands.js').then(r=>r.text()).then(eval)
-    createConsoleMenu();
+async function appendScript(url) {
+    const response = await fetch(url);
+    const code = await response.text();
+    
+    const script = document.createElement('script');
+    script.textContent = code;
+    document.head.appendChild(script);
+    
+    return new Promise(resolve => {setTimeout(resolve, 10)});
+}
+
+async function main() {
+    try {
+        await appendScript('https://raw.githubusercontent.com/Agrizok22507/BylbaConsole/main/gui.js');
+        await appendScript('https://raw.githubusercontent.com/Agrizok22507/BylbaConsole/main/execute.js');
+        await appendScript('https://raw.githubusercontent.com/Agrizok22507/BylbaConsole/main/comands.js');
+        createConsoleMenu();
+    } catch {
+        alert("Error with loading scripts")
+    }
 }
 
 main();
